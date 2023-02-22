b81bb70f0042137b0a988f150dca33e172bdd330
Mikinzi (frozencat3030) <112668967+mstrykul1336@users.noreply.github.com>
2023-02-22 15:48:33 -0700

    project

---
+++ Heroborn Book/Assets/Scripts/CombatManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : BaseManager
+{
+    public override string state
+    {
+        get { return _state; }
+        set {_state = value; }
+    }
+
+    public override void Initialize()
+    {
+        _state = "Manager intialized...";
+        Debug.Log(_state);
+    }
+}
---
+++ Heroborn Book/Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CustomExtensions;
+
+
+public class GameBehavior : MonoBehaviour
+{
+    private int _itemsCollected = 0;
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public int maxItems = 4;
+    public bool showWinScreen = false;
+    public bool showLossScreen = false;
+    private string _state;
+
+    public string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        _state = "Manager initialized...";
+        _state.FancyDebug();
+        Debug.Log(_state);
+    }
+    
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set {
+            _itemsCollected = value;
+            if(_itemsCollected >= maxItems) 
+            {
+                labelText = "You found all the items!";
+                showWinScreen = true;
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
+        }
+    }
+
+
+    private int _playerHP = 10;
+
+    public int HP
+    {
+        get { return _playerHP; }
+        set {
+            _playerHP = value;
+            if(_playerHP <= 0)
+            {
+                labelText = "You want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0;
+            }
+            else{
+                labelText = "Ouch...that's gotta hurt!";
+            }
+            Debug.LogFormat("Lives: {0}", _playerHP);
+        }
+    }
+
+
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect (20, 20, 150, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect (20, 50, 150, 25), "Items Collected:" + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if(showWinScreen)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 100, 100), "YOU WON!");
+            {
+                Utilities.RestartLevel(0);
+            }
+            //SceneManager.LoadScene(0);
+            //Time.timeScale = 1.0f;
+        }
+
+        if(showLossScreen)
+        {
+            if(GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You lose!"))
+            {
+                Utilities.RestartLevel(0);
+                //SceneManager.LoadScene(0);
+                //Time.timeScale = 1.0f;
+            }
+        }
+    
+    }
+}
